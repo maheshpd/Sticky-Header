08a93897740475cfa9e7736256c3e08e0f4a1445
Mahesh Prasad <mahesh223prasad@gmail.com>
2021-01-10 21:42:26 +0530

    Initial Commit

---
+++ Sticky Header/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Sticky Header
+//
+//  Created by Mahesh Prasad on 10/01/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Sticky Header/Sticky_HeaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sticky_HeaderApp.swift
+//  Sticky Header
+//
+//  Created by Mahesh Prasad on 10/01/21.
+//
+
+import SwiftUI
+
+@main
+struct Sticky_HeaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
